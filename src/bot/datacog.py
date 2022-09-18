02629a2e96a0025b9c7351fc0db18bce4e51c3dc
aloto <alotofcatz@gmail.com>
2022-09-18 07:07:05 -0700

    Fix label command not being able to accept post IDs. Improved method of getting new posts. Old method would cause the bot to continue checking for a new post, however by the time there was a new post, the bot context had generally expired, resulting in an error. The bot would also freeze until a new image was uploaded.
    This method gets a random post from the first page, and if those are all exhausted, continues on to the next page.

--- src/bot/datacog.py
+++ src/bot/datacog.py
@@ -1,3 +1,4 @@
+import random
 import traceback
 import pybooru
 import discord
@@ -8,6 +9,7 @@ from typing import Optional
 import json_interface
 
 embed_color = discord.Colour.from_rgb(215, 195, 134)
+page_number = 1
 
 
 class DataCog(commands.Cog, name='Data', description='Used by volunteers to label data.'):
@@ -15,6 +17,20 @@ class DataCog(commands.Cog, name='Data', description='Used by volunteers to labe
         self.bot = bot
         self.dbclient = pybooru.Danbooru('danbooru', username='haru1367')
         self.default_tags = 'rating:general'
+
+    def get_new_posts(self, tags):
+        global page_number
+        posts = self.dbclient.post_list(tags=tags, limit=25, page=page_number)
+        new_posts = []
+        for post in posts:
+            if json_interface.get_response(None, int(post['id'])) is None:
+                new_posts.append(post)
+        if len(new_posts) != 0:
+            return new_posts
+        else:
+            page_number += 1
+            return self.get_new_posts(tags)
+        # if there are no new posts, increase the page number and try again
 
     def get_post(self, tags=None, id=None):
         if id is not None:
@@ -24,7 +40,7 @@ class DataCog(commands.Cog, name='Data', description='Used by volunteers to labe
                 tags = self.default_tags
             else:
                 tags = tags + ' ' + self.default_tags
-            post = self.dbclient.post_list(tags=tags, limit=1)[0]
+            post = random.choice(self.get_new_posts(tags))
         return post
 
     async def post_searcher(ctx: discord.AutocompleteContext):
@@ -49,7 +65,7 @@ class DataCog(commands.Cog, name='Data', description='Used by volunteers to labe
                     autocomplete=post_searcher,
                     required=True
                     )
-    async def fetch(self, ctx: discord.ApplicationCommand, post_id: int = None):
+    async def fetch(self, ctx: discord.ApplicationContext, post_id: int = None):
         try:
             if post_id is not None: assert isinstance(post_id, int)
             post = self.get_post(None, post_id)
@@ -98,14 +114,8 @@ class DataCog(commands.Cog, name='Data', description='Used by volunteers to labe
             if post_id is not None:
                 # pycord hates us and gives us a string
                 post_id = int(post_id)
-
-            while True:
-                post = self.get_post(tags, post_id)
-                if json_interface.get_response(None, int(post['id'])):
-                    continue
-                else:
-                    break
-
+            # post = self.get_post(tags, post_id)
+            post = self.get_post(tags, post_id)
             embed = discord.Embed(color=embed_color)
             embed.set_footer(text=post['id'])
             embed.set_image(url=post['file_url'])
@@ -121,13 +131,24 @@ class DataCog(commands.Cog, name='Data', description='Used by volunteers to labe
                 embed.add_field(name='General', value=post['tag_string_meta'])
 
             post_info = f"Created At: ``{post['created_at']}``\nSize: ``{post['image_width']}``x``{post['image_height']}``\nScore: ``{post['score']}``"
-
             embed.add_field(name='Post Info', value=post_info, inline=False)
-
             await ctx.interaction.response.send_message(embed=embed)
         except Exception as e:
             embed = discord.Embed(title='Labeling failed.',
                                   description=f'An exception has occurred while fetching an image from Danbooru.\nError: {e}\n```{traceback.format_exc()}```',
+                                  color=embed_color)
+            await ctx.send_response(embed=embed, ephemeral=True)
+
+    @commands.slash_command(name='status', description='Show the dataset labeling status')
+    async def status(self, ctx: discord.ApplicationContext):
+        try:
+            embed = discord.Embed(color=embed_color)
+            data_amount = len(json_interface.get_responses(None).keys())
+            embed.add_field(name='Status', value=f'Currently we have {data_amount} caption image pairs!')
+            await ctx.interaction.response.send_message(embed=embed)
+        except Exception as e:
+            embed = discord.Embed(title='Status command failed.',
+                                  description=f'An exception has occurred while fetching the dataset status.\nError: {e}\n```{traceback.format_exc()}```',
                                   color=embed_color)
             await ctx.send_response(embed=embed, ephemeral=True)
 
